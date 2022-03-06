df213c7971dfea3d425f6ed203a678dcaabdecf6
Jason Leibowitz <jason@leibowitz.me>
2022-03-05 20:08:15 -0500

    Fix prettier issues

--- prettier.config.js
+++ prettier.config.js
@@ -1,4 +1,1 @@
-module.exports = {
-  singleQuote: true,
-  semi: true,
-};
+module.exports = {};
--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -1,4 +1,4 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
+import type { NextApiRequest, NextApiResponse } from "next";
 import { spotifyApi } from "utils/spotifyApi";
 import { generateRandomString } from "utils/string";
 
