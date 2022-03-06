eb305b7db433f1282c069abfe9c6fc8607fe88d1
Jason Leibowitz <jason@leibowitz.me>
2022-03-05 21:02:22 -0500

    Material UI Initial Implementation
    https://github.com/mui/material-ui/tree/master/examples/nextjs-with-typescript

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,33 @@
-import "../styles/globals.css";
+import Head from "next/head";
 import type { AppProps } from "next/app";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { CacheProvider, EmotionCache } from "@emotion/react";
+import createEmotionCache from "styles/createEmotionCache";
+import theme from "styles/theme";
 
-const MyApp = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />;
+// Client-side cache, shared for the whole session of the user in the browser
+const clientSideEmotionCache = createEmotionCache();
+
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
+}
+
+const MyApp = ({ Component, emotionCache = clientSideEmotionCache, pageProps }: MyAppProps) => (
+  <CacheProvider value={emotionCache}>
+    <Head>
+      <meta name="viewport" content="initial-scale=1, width=device-width" />
+    </Head>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  </CacheProvider>
+);
+
+// TODO: Is this needed if I pass it in when destructuring?
+MyApp.defaultProps = {
+  emotionCache: clientSideEmotionCache,
+};
 
 export default MyApp;
---
+++ src/styles/createEmotionCache.ts
@@ -0,0 +1,4 @@
+import createCache from "@emotion/cache";
+
+const createEmotionCache = () => createCache({ key: "css", prepend: true });
+export default createEmotionCache;
